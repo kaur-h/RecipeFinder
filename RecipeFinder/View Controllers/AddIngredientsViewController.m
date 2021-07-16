8ac7513fdd7aeef215eaf5cb613f1ed5ed44b7e4
kaur-h <harleenk120@gmail.com>
2021-07-16 15:25:51 -0400

    API Manager

--- RecipeFinder/View Controllers/AddIngredientsViewController.m
+++ RecipeFinder/View Controllers/AddIngredientsViewController.m
@@ -6,13 +6,17 @@
 //
 
 #import "AddIngredientsViewController.h"
+#import "Ingredient.h"
+#import "APIManager.h"
+#import "IngredientCell.h"
 
-@interface AddIngredientsViewController ()
+@interface AddIngredientsViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITextField *nameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *quantityTextField;
 @property (weak, nonatomic) IBOutlet UITextField *categoryTextField;
 @property (weak, nonatomic) IBOutlet UIButton *addButton;
-
+@property (strong,nonatomic) UITableView *autocompleteTableView;
+@property (strong, nonatomic) NSArray *autocompleteResults;
 @end
 
 @implementation AddIngredientsViewController
@@ -21,19 +25,42 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.addButton.layer.cornerRadius = 5;
+    self.autocompleteTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 80, 320, 120) style:UITableViewStylePlain];
+    self.autocompleteTableView.delegate = self;
+    self.autocompleteTableView.dataSource = self;
+    self.autocompleteTableView.scrollEnabled = YES;
+    self.autocompleteTableView.hidden = YES;
+    [self.view addSubview:self.autocompleteTableView];
+    
 }
 - (IBAction)addTapped:(id)sender {
     [self dismissViewControllerAnimated:true completion:nil];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)endEditingIngredientName:(id)sender {
+    NSLog(@"Editing End");
 }
-*/
+
+
+- (IBAction)editingIngredientName:(id)sender {
+    NSLog(@"ingredient name changed");
+    [[APIManager shared] getAutoCompleteIngredientSearch:^(NSDictionary *ingredients, NSError *error){
+            if(error){
+                NSLog(@"Error");
+            }
+            else{
+                NSLog(@"Here: %@", ingredients);
+            }
+        }];
+}
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    return [[UITableViewCell alloc] init];
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
 
 @end
