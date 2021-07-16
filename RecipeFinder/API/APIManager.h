31913649effea140aaeb6330687cd7fb507cbf0f
kaur-h <harleenk120@gmail.com>
2021-07-16 15:56:50 -0400

    APIManager

--- RecipeFinder/API/APIManager.h
+++ RecipeFinder/API/APIManager.h
@@ -11,8 +11,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface APIManager : NSObject
 + (instancetype)shared;
+<<<<<<< Updated upstream
 
 - (void)getAutoCompleteIngredientSearch:(void(^)(NSDictionary *ingredients, NSError *error))completion;
+=======
+<<<<<<< HEAD
+- (void)getAutocompleteIngredients:(void(^)(NSDictionary *ingredients, NSError *error))completion;
+=======
+
+- (void)getAutoCompleteIngredientSearch:(void(^)(NSDictionary *ingredients, NSError *error))completion;
+>>>>>>> 4bb776a70622a92f71178b709f7107de67df73ef
+>>>>>>> Stashed changes
 @end
 
 NS_ASSUME_NONNULL_END
