abb8effa0d15ac7d87a79ad664b80f7b1b6ff8b6
kaur-h <harleenk120@gmail.com>
2021-07-29 13:15:30 -0400

    User Profile Image

--- RecipeFinder/View Controllers/ProfileViewController.m
+++ RecipeFinder/View Controllers/ProfileViewController.m
@@ -10,12 +10,12 @@
 #import "LoginViewController.h"
 #import "SceneDelegate.h"
 
-@interface ProfileViewController ()
+@interface ProfileViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *ingredientCountLabel;
 @property (weak, nonatomic) IBOutlet UILabel *favoriteCountLabel;
 @property (weak, nonatomic) IBOutlet UIButton *logoutButton;
-@property (weak, nonatomic) IBOutlet PFImageView *profileImage;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
 
 @end
 
@@ -25,6 +25,15 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.logoutButton.layer.cornerRadius = 4;
+    self.profileImage.layer.cornerRadius = 80;
+    
+    PFUser *user = [PFUser currentUser];
+    PFFileObject *userProfileFile = user[@"profileImage"];
+    if(userProfileFile){
+        NSData *imageData = [userProfileFile getData];
+        self.profileImage.image = [[UIImage alloc] initWithData:imageData];
+    }
+    
 }
 - (IBAction)logoutTapped:(id)sender {
     //Logging out user
@@ -39,5 +48,47 @@
     myDelegate.window.rootViewController = loginViewController;
 }
 
+- (IBAction)imageTapped:(id)sender {
+    NSLog(@"Image Clicked");
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use a
+    self.profileImage.image = editedImage;
+    NSData *imageData = UIImageJPEGRepresentation(editedImage, 1.0);
+    PFFileObject *imageFile = [PFFileObject fileObjectWithName:@"image.png" data:imageData];
+    [imageFile saveInBackground];
+
+    PFUser *user = [PFUser currentUser];
+    [user setObject:imageFile forKey:@"profileImage"];
+    [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            NSLog(@"Successfully changed profile picture!");
+        }
+        else{
+            NSLog(@"Error %@", error.localizedDescription);
+        }
+    }];
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
